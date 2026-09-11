05c0ef327429b4f4c203b73d60631626f1fb175b
Thomas <thomas.erents@gmail.com>
2026-09-11 20:58:30 +0200

    Completely initialize Hera

--- hosts/hera.nix
+++ hosts/hera.nix
@@ -1,7 +1,30 @@
+let
+  mainIface = "ens192";
+in
 {
-  imports = [
-    # keep-sorted start
-    ../base/base.nix
-    # keep-sorted end
-  ];
+  _module.args = {
+    inherit mainIface;
+  };
+  imports =
+    let
+      staticConf = {
+        hostName = "hera";
+        domain = "16.dapperepoging.nl";
+        iface = mainIface;
+        ipv4Address = "10.0.116.8";
+        ipv6Address = "2a07:54c1:4932:116::8";
+        defaultGateway = "10.0.116.1";
+        defaultGateway6 = "fe80::c1c0";
+      };
+    in
+    [
+      # keep-sorted start
+      (import ../networking/client/static.nix { conf = staticConf; })
+      ../base/base.nix
+      ../monitoring/zabbix-agent.nix
+      ../monitoring/zabbix-server.nix
+      ../networking/client/resolv.conf.nix
+      ../networking/client/ssh.nix
+      # keep-sorted end
+    ];
 }
