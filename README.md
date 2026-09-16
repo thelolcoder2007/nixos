63b0ec99ddf6749f65c22ea0a56b056c7e30c2e9
Thomas <thomas.erents@gmail.com>
2026-09-16 21:20:17 +0200

    Implement postgres.nix

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## TODO
 
 - [x] Make nginx work modularly without weird `(import ./bullshit.nix)` shennanigans
-- [ ] Implement database/postgres.nix
+- [x] Implement database/postgres.nix
 - [x] Implement networking/server/HTTP/defaults.nix
 - [x] Implement Poseidon
 - [x] Find a better place for networking/server/BGP/DN42-cert.nix
--- database/postgres.nix
+++ database/postgres.nix
@@ -1,3 +1,47 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
+{
+  imports = [
+    ../base/sops.nix
+  ];
+  sops.secrets."database_monitor_password".restartUnits = [
+    "postgresql.service"
+  ];
+  services.postgresql = {
+    enable = true;
+    initialScript = pkgs.writeText "zabbix-monitor-setup.sql" ''
+      CREATE USER zbx_monitor WITH PASSWORD $(${config.sops.secrets."database_monitor_password".path});
+      GRANT pg_monitor TO zbx_monitor;
+    '';
+
+    authentication = lib.mkForce ''
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+      local   all             postgres                                peer
+      local   all             zabbix                                  trust
+      host    all             zabbix          127.0.0.1/32            md5
+    '';
+  };
+  services.zabbixAgent = {
+
+    # Monitoring Postgres server
+    extraPackages = with pkgs; [
+      zabbix-agent2-plugin-postgresql
+    ];
+    settings = {
+      "Plugins.PostgreSQL.System.Path" = lib.getExe pkgs.zabbix-agent2-plugin-postgresql;
+
+      "Plugins.PostgreSQL.Sessions.postgres.Uri" = "tcp://localhost:5432";
+      "Plugins.PostgreSQL.Sessions.postgres.User" = "zabbix";
+      "Plugins.PostgreSQL.Sessions.postgres.Password" = "$(cat ${
+        config.sops.secrets."database_monitor_password".path
+      })";
+      "Plugins.PostgreSQL.Sessions.postgres.Database" = "postgres";
+    };
+  };
 }
